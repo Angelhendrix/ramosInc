cb92ad70c78b5a1a38bd701a9d9064199ff01af1
Angel Hernandez <ale12jr415@gmail.com>
2019-03-04 20:17:55 -0800

    trying to fixed the service page bugs

--- js/main.js
+++ js/main.js
@@ -359,7 +359,103 @@ function moveToSelected3(element3) {
   });
 
 
+$('#checkbox').change(function(){
+    setInterval(function () {
+        moveRight();
+    }, 2000);
+  });
+  
+  var slideCount = $('#slider ul li').length;
+  var slideWidth = $('#slider ul li').width();
+  var slideHeight = $('#slider ul li').height();
+  var sliderUlWidth = slideCount * slideWidth;
+  
+  $('#slider').css({ width: slideWidth, height: slideHeight });
+  
+  $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+  
+    $('#slider ul li:last-child').prependTo('#slider ul');
+
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    $('a.control_prev').click(function () {
+        moveLeft();
+    });
+
+    $('a.control_next').click(function () {
+        moveRight();
+    });
+
+
+
+var images = '.image-container',
+    slide = $(images+' img').innerWidth();
+
+function append(){$(images+' img').first().appendTo($(images));}
+function prepend(){$(images+' img').last().prependTo($(images));}
+
+$('.prev').click(function(){ prepend(); });
+$('.next').click(function(){ append();  });
+
+
+
+
+  var slideCount2 = $('#slider2 ul li').length;
+  var slideWidth2 = $('#slider2 ul li').width();
+  var slideHeight2 = $('#slider2 ul li').height();
+  var sliderUlWidth2 = slideCount2 * slideWidth2;
+  
+  $('#slider2').css({ width: slideWidth2+10, height: slideHeight2+90 });
+  
+  $('#slider2 ul').css({ width: sliderUlWidth2, marginLeft: - slideWidth2 });
+  
+    $('#slider2 ul li:last-child').prependTo('#slider2 ul');
+
+    function moveLeft2() {
+        $('#slider2 ul').animate({
+            left: + slideWidth2
+        }, 200, function () {
+            $('#slider2 ul li:last-child').prependTo('#slider2 ul');
+            $('#slider2 ul').css('left', '');
+        });
+    };
+
+    function moveRight2() {
+        $('#slider2 ul').animate({
+            left: - slideWidth2
+        }, 200, function () {
+            $('#slider2 ul li:first-child').appendTo('#slider2 ul');
+            $('#slider2 ul').css('left', '');
+        });
+    };
+
+    $('a.control_prev2').click(function () {
+        moveLeft2();
+    });
+
+    $('a.control_next2').click(function () {
+        moveRight2();
+    });
+
   //contact form//
+
+  
 
 
 });
